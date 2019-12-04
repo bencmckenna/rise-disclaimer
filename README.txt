ceafc926b73e1d579aa44edbf058c3a3daf16aef
bencmckenna <47491782+bencmckenna@users.noreply.github.com>
2019-12-04 10:17:24 +1000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+To add disclaimer:
+1. Unzip your Rise course
+2. Copy the disclaimer.js & disclaimer.txt files to [RISE COURSE]\scormcontent\ subfolder (there should be an index.html in this folder)
+3. Open disclaimer.txt in a text editor, change the text to match your disclaimer & save
+3. Open index.html in Notepad and add the following line to the <head> tags:
+	
+	<script type="text/javascript" src="disclaimer.js"></script>
+	
+4. Rezip your course and upload to the LMS
---
+++ disclaimer.js
@@ -0,0 +1,36 @@
+var disclaimerText = "";
+var disclaimerAdded = false;
+
+fetch('disclaimer.txt')
+  .then(response => response.text())
+  .then((data) => {
+    disclaimerText = data
+  })
+
+window.onload = function(){
+	disclaimerAdded = false;
+	checkExist();
+}
+
+window.onhashchange = function(){
+	if (window.location.href.split('/').pop() == ""){
+		disclaimerAdded = false;
+		checkExist();
+	};
+}
+
+function checkExist(){setInterval(function() {
+	if (disclaimerAdded == false){
+		if (document.getElementsByClassName("overview__container").length) {
+			  console.log("Exists!");
+			  var disclaimer = document.getElementsByClassName("overview__container")[0];
+			  disclaimer.innerHTML += "<br>";
+			  disclaimer.innerHTML += "<p style='font-size:8pt;'>" + disclaimerText + "</p>";
+			  disclaimerAdded = true;
+			  clearInterval(checkExist);
+			} else {
+			  console.log("Doesn't exist yet");
+		   }
+		}
+	}, 100); // check every 100ms
+}
